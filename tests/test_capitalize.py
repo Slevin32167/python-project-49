bad0bd2cdb1667bcdbb05231beb4bdec9262f7ad
Slevin32167 <slevin321678@gmail.com>
2025-12-02 15:07:31 +0500

    fix: remove extra blank lines (ruff E303)

--- tests/test_capitalize.py
+++ tests/test_capitalize.py
@@ -5,21 +5,16 @@ print("Тест 1: Проверяем пустую строку ...")
 assert capitalize("") == ""
 print("Пустая строка работает!")
 
-
 print("Тест 2: Проверяем слово 'hello'...")
 assert capitalize("hello") == "Hello"
 print("Слово 'hello' работает!")
-
 
 print("Тест 3: Проверяем слово 'world'...")
 assert capitalize("world") == "World"
 print("Слово 'world' работает!")
 
-
-print("Тест 3: Проверяем слово 'hexlet'...")
+print("Тест 4: Проверяем слово 'hexlet'...")
 assert capitalize("hexlet") == "Hexlet"
 print("Слово 'hexlet' работает!")
-
-
 
 print("Все тесты завершены!")
